24acb53aeddfc9a98ed1cf740fa71d461e59c978
EthanL247 <Ethanliu@Live.co.uk>
2024-01-19 14:36:33 +0000

    forms_wip

--- webapp.py
+++ webapp.py
@@ -6,10 +6,15 @@ app = Flask("App")
 
 
 @app.route('/voting')
-def hello():
+def voting():
     response = requests.get('http://localhost:5000/voting')
     data = json.loads(response.text)
     return render_template('voting.html',data=data)
+
+@app.route('/confirmation')
+def confirmation():
+    
+    pass
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=80)
